197db48c41f80da9d6517c2c7f13a01dabc78e9b
Ezli Cruz <soccaezli05@gmail.com>
2026-04-21 14:48:38 -0700

    finished RESERVATIONS

--- inn/reservations.py
+++ inn/reservations.py
@@ -5,19 +5,22 @@ with open("Reservations.csv", 'r', encoding= 'utf8') as file, open(f'Reservation
     reader = csv.reader(file)
     header = next(reader)
 
+    month_num = {'JAN': '01', 'FEB': '02', 'MAR': '03', 'APR': '04', 'MAY': '05', 'JUN': '06',
+                 'JUL': '07', 'AUG': '08', 'SEP': '09', 'OCT': '10', 'NOV': '11', 'DEC': '12'}
+
     for row in reader:
         values = []
-        month_num = {'JAN': '01', 'FEB': '02', 'MAR': '03', 'APR': '04', 'MAY': '05', 'JUN': '06',
-                     'JUL': '07', 'AUG': '08', 'SEP': '09', 'OCT': '10', 'NOV': '11', 'DEC': '12'}
-        for val in row:
-            val = val.strip(" ").strip("'")
+
+        for i in range(0,9):
+            val = row[i].strip(" ").strip("'")
             if val == "":
                 values.append('NULL')
-            for month in month_num:
-                if month in val:
-                    val = val.replace(month,month_num[month])
-                    values.append(f"STR_TO_DATE('{val}', '%d-%m-%y')")
-                    break
+            elif i == 2 or i == 3:
+                for month in month_num:
+                    if month in val.upper():
+                        val = val.replace(month,month_num[month])
+                        break
+                values.append(f"STR_TO_DATE('{val}', '%d-%m-%y')")
             else:
                 values.append(f"'{val}'")
 
