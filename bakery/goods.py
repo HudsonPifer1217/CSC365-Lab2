5efad982e4cbc6fdcc3852e0f05210126dcf91bc
Ezli Cruz <soccaezli05@gmail.com>
2026-04-21 15:02:22 -0700

    finished GOODS

---
+++ bakery/goods.py
@@ -0,0 +1,22 @@
+import csv
+table_name = 'GOODS'
+
+with open("goods.csv", 'r', encoding= 'utf8') as file, open(f'goods-build-{table_name}.sql', 'w', encoding='utf8') as outfile:
+    reader = csv.reader(file)
+    header = next(reader)
+
+    for row in reader:
+        values = []
+
+        for val in row:
+            val = val.strip(" ").strip("'")
+            if val == "":
+                values.append('NULL')
+            else:
+                values.append(f"'{val}'")
+
+        columns = ", ".join(header)
+        values_str = ", ".join(values)
+
+        sql = f"INSERT INTO {table_name} ({columns}) VALUES ({values_str});\n"
+        outfile.write(sql)
