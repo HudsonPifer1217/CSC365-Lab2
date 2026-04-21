b75138a30d11a1ef1edd8199ec3978e83634ce40
Ezli Cruz <soccaezli05@gmail.com>
2026-04-21 13:36:50 -0700

    finished RESERVATIONS

---
+++ inn/reservations.py
@@ -0,0 +1,29 @@
+import csv
+table_name = 'RESERVATIONS'
+
+with open("Reservations.csv", 'r', encoding= 'utf8') as file, open('Reservations-build-RESERVATIONS.sql', 'w', encoding='utf8') as outfile:
+    reader = csv.reader(file)
+    header = next(reader)
+
+    for row in reader:
+        values = []
+        month_num = {'JAN': '01', 'FEB': '02', 'MAR': '03', 'APR': '04', 'MAY': '05', 'JUN': '06',
+                     'JUL': '07', 'AUG': '08', 'SEP': '09', 'OCT': '10', 'NOV': '11', 'DEC': '12'}
+        for val in row:
+            val = val.strip(" ").strip("'")
+            if val == "":
+                values.append('NULL')
+            for month in month_num:
+                if month in val:
+                    val = val.replace(month,month_num[month])
+                    values.append(f"'{val}'")
+                    break
+            else:
+                val = val.strip(" ").strip("'")
+                values.append(f"'{val}'")
+
+        columns = ", ".join(header)
+        values_str = ", ".join(values)
+
+        sql = f"INSERT INTO {table_name} ({columns}) VALUES ({values_str});\n"
+        outfile.write(sql)
