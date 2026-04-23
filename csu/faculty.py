f68fc511c501943b5969675c84fe1995bc039529
HudsonPifer1217 <hupifer@calpoly.edu>
2026-04-22 22:53:36 -0700

    Completed CSU files

---
+++ csu/faculty.py
@@ -0,0 +1,23 @@
+import csv
+int_colums = {"Campus","Year","Faculty"}
+table_name = "FACULTY" 
+
+with open("faculty.csv", "r", encoding="utf8") as file, open(f"build-{table_name}.sql", "w", encoding="utf8") as outfile:
+    reader = csv.reader(file, skipinitialspace=True)
+    header = next(reader)
+    for row in reader:
+        values = []
+        for col, val in zip(header, row):
+            val = val.strip().strip("'")
+            if val == "":
+                values.append("NULL")
+            elif col in int_colums:
+                values.append(val)
+            else:
+                val = val.replace("'", "''")
+                values.append(f"'{val}'")
+        columns= ", ".join(header)
+        values_str = ", ".join(values)
+        sql = f"INSERT INTO {table_name} ({columns}) VALUES ({values_str});\n"
+        outfile.write(sql)
+print(f"Done — written to build-{table_name}.sql")
